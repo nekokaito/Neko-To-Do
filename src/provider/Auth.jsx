762b98064000fa710e8684c9ca4f8661173ee453
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-12-09 16:46:20 +0600

    Firebase Config Done

---
+++ src/provider/Auth.jsx
@@ -0,0 +1,17 @@
+import { app } from "../firebase/firebase";
+
+//Global Var
+
+
+
+const Auth = () => {
+
+
+     return (
+          <div>
+               
+          </div>
+     );
+};
+
+export default Auth;
