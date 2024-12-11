f16f47ea150e1bc6749dd40df9bed215ef24a865
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-12-11 22:48:00 +0600

    handleEdit

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,45 @@ import axios from "axios";
 
 
 const App = () => {
-  
+
   const [todos, setTodos] = useState([]);
 
 
   const handleToDo = async (e) => {
-     e.preventDefault();
+    e.preventDefault();
 
     const inputTodo = e.target.todo_title.value;
-     
-    
-   await axios.post('http://localhost:4000/add-todo', { title: inputTodo }).then(res => console.log(res.data))
+
+
+    await axios.post('http://localhost:4000/add-todo', { title: inputTodo }).then(res => console.log(res.data))
 
     e.target.todo_title.value = "";
+    location.reload();
   }
+
+  useEffect(() => {
+    const getData = async () => {
+      await axios.get('http://localhost:4000/todos').then(res => setTodos(res.data))
+    }
+    getData()
+  }, [])
+
   
-  useEffect(()=> {
-    
-  },[])
 
   return (
     <div className="flex mt-20 flex-col items-center justify-center ">
-      <div className="flex gap-3">
-        <form onSubmit={handleToDo}>
-        <input name="todo_title" className="input border-white" type="text" />
-        <button className="btn" type="submit">Create</button>
+      <div className="">
+        <form className="flex gap-5" onSubmit={handleToDo}>
+          <input name="todo_title" className="input border-white" type="text" />
+          <button className="btn" type="submit">Create</button>
         </form>
       </div>
-      <div className="mt-10">
+      <div className="mt-10 flex flex-col gap-5">
+        {
+          todos.map(todo => <ToDoCard key={todo._id} todo={todo}></ToDoCard>)
+        }
 
-        <ToDoCard></ToDoCard>
+        
 
       </div>
     </div>
--- src/components/ToDoCard.jsx
+++ src/components/ToDoCard.jsx
@@ -1,11 +1,38 @@
+import { useState } from "react";
+import { FaRegEdit } from "react-icons/fa";
+import { MdOutlineDeleteForever } from "react-icons/md";
 
 
-const ToDoCard = () => {
+const ToDoCard = ({ todo }) => {
+     const { _id, title } = todo;
+
+     const [newTitle, setNewTitle] = useState(title);
+
+
+
+     const handleEdit = async (e) => {
+          const updatedTitle = e.target.innerText;
+          setNewTitle(updatedTitle); 
+     
+     }
+
+     console.log(newTitle);
+
+     
+
+
+
      return (
-          <div>
-
-            <h1 className="text-2xl">Dynamic</h1>
-
+          <div className="card bg-primary text-primary-content flex justify-center items-center w-96 h-20">
+               <div className="flex gap-6">
+                    <h1 className="text-2xl w-40" contentEditable="true" suppressContentEditableWarning={true} 
+                         onBlur={handleEdit}>{title}</h1>
+                    <div className="">
+                         
+                         <button className="btn"><MdOutlineDeleteForever /></button>
+                         
+                    </div>
+               </div>
           </div>
      );
 };
