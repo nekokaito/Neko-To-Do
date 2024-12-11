379d6c84385a6fe42c57f6179bf3c9f61058483a
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-12-11 22:12:16 +0600

    axios post and handleToDo

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
+import { useEffect, useState } from "react";
 import ToDoCard from "./components/ToDoCard"
+import axios from "axios";
 
 
 const App = () => {
+  
+  const [todos, setTodos] = useState([]);
 
-  const handleToDo = () => {
-    console.log('todo handle');
+
+  const handleToDo = async (e) => {
+     e.preventDefault();
+
+    const inputTodo = e.target.todo_title.value;
+     
+    
+   await axios.post('http://localhost:4000/add-todo', { title: inputTodo }).then(res => console.log(res.data))
+
+    e.target.todo_title.value = "";
   }
-
+  
+  useEffect(()=> {
+    
+  },[])
 
   return (
     <div className="flex mt-20 flex-col items-center justify-center ">
       <div className="flex gap-3">
-        <input className="input border-white" type="text" />
-        <button className="btn" onClick={handleToDo}>Create</button>
+        <form onSubmit={handleToDo}>
+        <input name="todo_title" className="input border-white" type="text" />
+        <button className="btn" type="submit">Create</button>
+        </form>
       </div>
       <div className="mt-10">
 
